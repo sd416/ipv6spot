01b3ff371bf295b8e5e334b21eeb4f7a606028a8
Abdulkader Alrezej <34913203+abdulkader-alrezej@users.noreply.github.com>
2024-09-21 12:24:35 +0300

    IPv6Spot Main Code of System

--- tv.py
+++ tv.py
@@ -11,9 +11,7 @@ from datetime import datetime
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
-
 # Abdulkader Alrezej
-
 os.environ['FLASK_ENV'] = 'production'
 app = Flask(__name__, template_folder='/mnt/cerr/web_dist/html_page', static_folder='/mnt/cerr/web_dist/img')
 app.secret_key = os.environ.get('SECRET_KEY', '0000')
